f8b49f9df88573287d0b9658730642184de89604
siddharth padhi <padhisiddharth31@gmail.com>
2022-12-26 22:15:25 +0530

    prod push

--- handler.js
+++ handler.js
@@ -4,28 +4,11 @@ var expressions = require("./utils/regex");
 module.exports.hello = async (event) => {
   const {email , appName} = event
 // const {email , appName} = {
-//   email: `[http://l.flipkart.com/t/open/Pqt5dGC5tS9HKrZzbsZoVI7MNe8B2zIOaIvOqDOaC_5jG_br77Pl1XYUs214iGawCG-v5NWjP_yAwuLdKUp0VmbypM4BJ9mJx0IodGJjuqN3j9uKgTYxjWk9-aw-gonQdGr7YMoNo9yCPEx8pC57B41KSVWcNm9Tlm76gofVCdbLF_tcvk8pAXbkKgc6oqwxUcczdVqtCeLS4BFXGLPCKZaHjFStykN7L76bP0ZVdhJA2HRS-JYAtSVcm3hwM-9kt3gzknh8fxKy1I05zHuHy38_ePgjSNsr3rffxayPSOyMTNkb66Cou3rBsqEPfsZpRmwE8BSF6bq_WGTYarRSqQ==?e=true]
-
-//   Flipkart.com
-//   [https://img1a.flixcart.com/www/email/images/20170217-182335-2017-02-17.png]
-//   [http://l.flipkart.com/t/click/ioUdrLOVu4babyFxhzpH7TjGynp8L5H_QOCxupKAfugs6ToZANjbd5IYNrFvnfwfOYsBx6julLe3AnEWsOsB2dflAA5Js8BDJirrsoaMhKpFuva2KdNwoW8gYPS9gfbzBaPPG8vekuIIKeJQX5wxwcc97FFxPYfd1QkK17BNkDn5QGBhPoryXwtFVrWiV6fEBjlBRMYDN_zryyVgYeXFj9MxchaGJFHIHybB_iY5Rdyw3Ef9jM5BPqW-0M3FInNCLhXsjvV-swamdfmCwPoZCo6eTfzFucB-WuaNLdmzL_MGt4wz0lODGcCJnn_xl7U18Qf_5rdRK0GZT6tiIcj6q0RFzD5tDHnGUJTPrYd3mxjwb372QFzYEZUF99i7JyAxnF64tsbyONogQD7832Hhl4lmeEmljHMSPNUwPBAwCMHixFg12l7kWs_zoBSGEkK8dopyf4TClroM0zQ1QWWr0EnkAGN6OVyD3gX_I41cj1oFl4A07aO_Rl6CcdE6oqJdabYYmVtIz-gHXksWmwuXzw==?e=true]
-  
-//   Hi Biswajit
-  
-//   Greetings from Flipkart.
-  
-//   Thanks for using Flipkart Pay Later. We've received Rs 199.0 towards your August
-//   bill.
-  
-  
-  
-//   We would love to get your feedback.
-  
-//   How was your Flipkart Pay Later bill payment experience?`,
-//     appName:'Flipkart Pay Later'
+//   email: `HI sonali, ₹265.74 HAS BEEN RECEIVED FOR YOUR SIMPL ACCOUNT ON 3RD SEPTEMBER 2022`,
+//     appName:'simpl'
 //   }
   
-  a = email.replace(/\*|_/g, "").replace(/\<|_/g, "").replace(/\>|_/g, "").replace(/\r\n|\r|\n|_/g, " ").replace(/\s\s+/g, ' ').replace(/[\[\]]+/g,'')
+  a = email.replace(/\*|_/g, "").replace(/\<|_/g, "").replace(/\>|_/g, "").replace(/\r\n|\r|\n|_/g, " ").replace(/\s\s+/g, ' ').replace(/[\[\]]+/g,'').replace(/\--|_/g, "")
   console.log(a)
                         const reg = expressions.expressionData[appName].regex;
                         //console.log(reg)
--- utils/regex.js
+++ utils/regex.js
@@ -1,5 +1,5 @@
 module.exports.expressionData = {
-    listOfApps: ["flip"],
+    listOfApps: ["freecharge"],
     'Flipkart Pay Later': {
       regex: [
         "dues\\sfor\\s([a-zA-Z]*)\\s([0-9]{4})\\sby\\s([0-9]{2})\\s([a-zA-Z]*)\\s([0-9]{4})",
@@ -11,17 +11,62 @@ module.exports.expressionData = {
         ["bill_month","year", "bill_due_date", "bill_due_month","bill_due_year"],
         ["currency", "amount_due"],
         ["pay_url"],
-        ["paid_amount","bill_month"]
+        ["paid_amount","bill_paid_month"]
       ],
     },
     simpl:{
       regex: [
         "([0-9]{2})\\s(jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\\s([0-9]{4})\\s([^a-zA-Z])([0-9]*.[0-9]*)",
-        "Pay\\s{1,}Bill\\s{1,}(http[s]?:\\/?\\/?[^:\\/\\s]+(\\/\\w+)*\\/[\\w\\-\\.]+[^#?\\s]+.*?(#[\\w\\-]+)?)"
+        "Pay\\s{1,}Bill\\s{1,}(http[s]?:\\/?\\/?[^:\\/\\s]+(\\/\\w+)*\\/[\\w\\-\\.]+[^#?\\s]+.*?(#[\\w\\-]+)?)",
+        "hi\\s(\\w+),\\s(.)(\\d{1,}.\\d{1,})is\\sdue\\sby\\s([0-9]{2})\\s(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)\\s(\\d{4})\\sfor\\spurchases",
+        "hi\\s(\\w+\\s\\w+|\\w+),\\s(.)(\\d{1,}.\\d{1,})\\shas\\sbeen\\sreceived\\sfor\\syour\\ssimpl\\saccount\\son\\s(\\d{1,2})(?:th|rd)\\s(january|february|march|april|may|june|july|august|september|october|november|december)\\s(\\d{4})"
       ],
       returnData: [
         ["bill_due_date","bill_due_month","bill_due_year","currency","amount_due"],
-        ["pay_url"]
+        ["pay_url"],
+        ['first_name','currency','amount_due','bill_due_date','bill_due_month','bill_due_year'],
+        ['first_name','currency','paid_amount','bill_paid_date','bill_paid_month','bill_paid_year']
+      ]
+    },
+    'Amazon Pay Later':{
+      regex:[
+        "An\\sAmount\\sof\\s(Rs)\\s(\\d{1,}.\\d{1,})", 
+        "account\\sno.\\s(X+[0-9]+)\\son\\s(\\d{2})-(\\d{2})-(\\d{2})",
+        //  "click here\\s(https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&/=$]+)*)\\s",
+        'a\\sreceipt\\sof\\s([a-z A-Z]*)\\s([0-9]+.[0-9]+)\\smade\\svia\\sdebit\\scard\\/netbanking\\son\\s(\\d{2})-(\\d{2})-(\\d{2})'
+      ],
+      returnData:[
+        ["currency","amount_due",],
+        ["accountNumber", "bill_due_date","bill_due_month","bill_due_year"],
+        ["currency", "paid_amount","bill_paid_date","bill_paid_month","bill_paid_year"]
+      ]
+    },
+    slice:{
+      regex:[
+        "Your\\srepayment\\sof\\s(.)([0-9]+(?:,[0-9]+)*)",
+        "Your\\stotal\\sdue\\samount\\sfor\\s(\\d{1,2})th\\s([a-zA-Z]+)\\sis\\s(.)([0-9]+,(?:[0-9]+)*)",
+        "Repay\\snow\\s(https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&/=$]+)*)"
+      ],
+      returnData:[
+
+        ['currency','paid_amount'],
+        ['bill_due_date','bill_due_month','currency','amount_due'],
+        ['pay_url']
+      ]
+    },
+    freecharge:{
+      regex:[
+        "Total\\sDue\\sAmount\\s(.)(\\d{1,}.\\d{1,})",
+        "Due\\sDate\\s(\\d{2})-(\\d{2})-(\\d{4})",
+        "PAY\\sBILL\\s(https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b[-a-zA-Z0-9()@:%_\\+.~#?&//=]*)",
+        "Total\\sDue\\sAmount\\sPaid\\s(.)(\\d{1,}.\\d{1,})\\s+Bill\\sPayment\\sDate\\s(\\d{1,2})-(\\d{1,2})-(\\d{1,4})"
+      ],
+      returnData:[
+        ['currency','amount_due',],
+        ['bill_due_date','bill_due_month','bill_due_year'],
+        ['pay_url'],
+        ["currency","paid_amount","bill_paid_date","bill_paid_month","bill_paid_year"]
+
       ]
     }
   };
